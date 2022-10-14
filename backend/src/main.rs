1bc85b59e9bb47945f340a5508e7670be7cc5716
svicervlad <vlad.sadretdinov@jet.dev>
2022-10-14 21:19:26 +0000

    feat: configure rocket to serve yew app features:   - add ports and 0.0.0.0 to serve buffs   - add rank(1) to serve to fix wasm headers

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,11 +1,14 @@
 #[macro_use] extern crate rocket;
+use rocket::fs::{FileServer, relative};
 
-#[get("/<name>")]
+#[get("/<name>", rank = 2)]
 fn index(name: &str) -> String {
     format!("Hello, {}!", name)
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build()
+    .mount("/", FileServer::from(relative!("../dist")).rank(1))
+    .mount("/", routes![index])
 }
